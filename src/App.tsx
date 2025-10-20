2d25ac8467afcf4d4447f479abfde806a38eca0e
chrisdesousafreitas <chrissf0211@gmail.com>
2025-10-20 20:23:45 -0300

    feat: import das rotas 'Home' e 'Login'

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Cabecalho } from "./Components/Cabecalho/Cabecalho";
 import { Rodape } from "./Components/Rodape/Rodape";
-
+import Home from "./routes/Home/Index";
+import Login from "./routes/Login/Index";
 function App() {
 
   
