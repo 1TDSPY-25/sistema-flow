98dfd43b4a4fc6a2af8d3304d868107466bbca69
Rodrigotiezzi <rm562975@fiap.com.br>
2025-10-20 20:48:39 -0300

    chore: Criação das rotas na Main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "../globals.css"
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './routes/Home/Index.tsx'
+import Cadastro from './routes/Cadastro/Index.tsx'
+import Error from './routes/Error/Index.tsx'
+import Login from './routes/Login/Index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+const router = createBrowserRouter([
+  {
+    path:"/", element: <App/>, errorElement: <Error/>, children:[
+     {path:"/", element: <Home/>},
+     {path:"/login", element: <Login/>},
+     {path:"/cadastro", element: <Cadastro/>}
+    ]     
+  }
+])
