b7089787dbd275923145bee4433f9df220a28a0d
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-10-23 22:20:34 -0300

    feat: App preparado para dark-mode.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-
-
-
+import { Outlet } from "react-router-dom"
+import { Cabecalho } from "./Components/Cabecalho/Cabecalho"
+import { Rodape } from "./Components/Rodape/Rodape"
+import { useTheme } from "./context/usetheme";
 
 function App() {
+  const { isDark } =  useTheme();
   return (
-    <div>App</div>
+    <div className={`${isDark ? 'dark-theme': 'light-theme'} min-h-screen flex flex-col w-full h-full justify-between`}>
+      <Cabecalho/>
+          <Outlet/>
+      <Rodape/>
+    </div>
   )
 }
 
