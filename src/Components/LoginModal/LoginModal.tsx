72c6319bdc95bf37a13093ed808420904abbc4bb
JvSantanaSantos <joao.santanadossanto07@gmail.com>
2025-10-29 17:05:56 -0300

    feat: Cria arquivo LoginModal.tsx

---
+++ src/Components/LoginModal/LoginModal.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface LoginModalProps {
+  onClose: () => void;
+  onNavigateToRegister: () => void;
+}
+
+export function LoginModal({ onClose, onNavigateToRegister }: LoginModalProps) {
+  
+  return (
+    <div>
+      {}
+    </div>
+  );
+}
