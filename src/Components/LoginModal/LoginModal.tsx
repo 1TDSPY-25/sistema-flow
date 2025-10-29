bace95c2cd437c25b12e398ec4a1536f6617ae9e
FelipeRibeiroSalles <feliperscamargo@gmail.com>
2025-10-29 17:33:35 -0300

    feat: implementando overlay do modal

--- src/Components/LoginModal/LoginModal.tsx
+++ src/Components/LoginModal/LoginModal.tsx
@@ -8,8 +8,20 @@ interface LoginModalProps {
 export function LoginModal({ onClose, onNavigateToRegister }: LoginModalProps) {
   
   return (
-    <div>
-      {}
+    import React from 'react';
+
+interface LoginModalProps {
+  onClose: () => void;
+  onNavigateToRegister: () => void;
+}
+
+export function LoginModal({ onClose, onNavigateToRegister }: LoginModalProps) {
+  
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+
     </div>
+  );
+}
   );
 }
