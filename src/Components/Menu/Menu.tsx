caba2d1ead2dbb31a5cd9af7b51eff989e78e2c7
pamellachristiny <pamella.christiny2006@gmail.com>
2025-10-29 20:44:40 -0300

    chore: Arrumando a posição e desing do cabeçalho.

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -2,10 +2,49 @@ import { Link } from "react-router-dom";
 
 export function Menu() {
   return (
-    <nav className="menu">
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/cadastro">Cadastro</Link>
+    <nav
+      className="
+        flex items-center justify-center gap-8 
+        px-6 py-3 
+        bg-white shadow-sm rounded-lg 
+        text-sm font-medium
+      "
+    >
+      {/* Home */}
+      <Link
+        to="/"
+        className="
+          text-gray-700 font-semibold 
+          hover:text-red-600 hover:underline underline-offset-4 
+          transition duration-200
+        "
+      >
+        Home
+      </Link>
+
+      {/* Cadastro */}
+      <Link
+        to="/cadastro"
+        className="
+          text-gray-700 font-semibold 
+          hover:text-red-600 hover:underline underline-offset-4 
+          transition duration-200
+        "
+      >
+        Cadastro
+      </Link>
+
+      {/* Login */}
+      <Link
+        to="/login"
+        className="
+          text-gray-700 font-semibold 
+          hover:text-red-600 hover:underline underline-offset-4 
+          transition duration-200
+        "
+      >
+        Login
+      </Link>
     </nav>
   );
 }
